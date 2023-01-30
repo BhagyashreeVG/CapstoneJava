8d90b58005a8652e9afa7338a7b069f8029a54d4
BhagyashreeVG <bhagyashreeghatapandi@gmail.com>
2023-01-30 13:25:30 +0000

    appointment service

---
+++ appointment-service/src/main/java/com/wecare/api/AppointmentController.java
@@ -0,0 +1,31 @@
+package com.wecare.api;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wecare.response.AppointmentResponse;
+import com.wecare.service.AppointmentService;
+
+@CrossOrigin(origins="*")
+@RestController
+@RequestMapping
+public class AppointmentController {
+
+	@Autowired
+	AppointmentService service;
+	
+	
+	@GetMapping("/coach/{id}")
+	public List<AppointmentResponse> getCoach(@PathVariable int id) {
+		return service.getAppointment(id);
+	}
+}
